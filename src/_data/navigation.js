d1425131984f19c22af821eed927afb316f3c891
robjwood <treborwood@gmail.com>
2024-04-20 08:39:47 +0100

    Initial commit

---
+++ src/_data/navigation.js
@@ -0,0 +1,20 @@
+module.exports = {
+  top: [
+    {
+      text: 'Elements',
+      url: '/elements/',
+    },
+    {
+      text: 'Components',
+      url: '/components/'
+    },
+    {
+      text: 'Section',
+      url: '/section/'
+    },
+    {
+      text: 'Blog',
+      url: '/blog/'
+    },
+  ]
+};
